1d61ba6d123cf0542e5ffaeece1c5f3d7f8a2373
SalikhovD <salikhvdima@gmail.com>
2022-09-07 16:06:42 +0300

    Исправление замечаний

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,56 +3,17 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
-
-import jm.task.core.jdbc.model.User;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
-
 
 public class Util {
 
-    private static final String dbHost = "localHost";
-    private static final String dbPort = "3306";
-    private static final String dbUser = "root";
-    private static final String dbPass = "root";
-    private static final String dbName = "katatraining";
-
-
-    private static SessionFactory factory = null;
+    private static final String DB_HOST = "localHost";
+    private static final String DB_PORT = "3306";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "root";
+    private static final String DB_NAME = "katatraining";
 
     public static Connection getDbConnection() throws SQLException, ClassNotFoundException {
-        String connectionString = "jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbName;
-        return DriverManager.getConnection(connectionString, dbUser, dbPass);
-    }
-
-    public static Session getSession() {
-        if (factory == null) {
-            try {
-                Configuration configuration = new Configuration();
-                Properties properties = new Properties();
-                properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                properties.put(Environment.URL, "jdbc:mysql://localhost:3306/katatraining?useSSL=false");
-                properties.put(Environment.USER, "root");
-                properties.put(Environment.PASS, "12345");
-                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
-                properties.put(Environment.SHOW_SQL, "true");
-                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-                configuration.setProperties(properties);
-                configuration.addAnnotatedClass(User.class);
-
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties()).build();
-
-                factory = configuration.buildSessionFactory(serviceRegistry);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return factory.getCurrentSession();
+        String connectionString = "jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
+        return DriverManager.getConnection(connectionString, DB_USER, DB_PASS);
     }
 }
